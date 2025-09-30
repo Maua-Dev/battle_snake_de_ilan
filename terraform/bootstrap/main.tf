292ff9f4fdf56167da356a6f03730be5349ba576
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-09-30 10:52:11 -0300

    quick fix updating to new aws account

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "s3" {
-    bucket         = "battle-snake-bootstrap-state"
-    dynamodb_table = "battle-snake-bootstrap-state-table"
-    region         = "us-west-2"
+    bucket         = "battle-snake-bootstrap-state-new-account"
+    dynamodb_table = "battle-snake-bootstrap-state-table-new-account"
+    region         = "us-east-1"
     encrypt        = true
   }
   required_providers {
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2" 
+  region = "us-east-1" 
 }
 
 variable "project_name" {
@@ -31,7 +31,7 @@ variable "environment" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "battlesnake-java-template-terraform-state-tfstate"
+  bucket = "battlesnake-java-template-terraform-state-tfstate-new-account"
 
   lifecycle {
     prevent_destroy = true
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_public_access_block" "block_public" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "battlesnake-java-template-terraform-locks-tfstate"
+  name         = "battlesnake-java-template-terraform-locks-tfstate-new-account"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
