bbf36338e5f340a6e9f8d67f4aeeb237698afebf
Ilan-Ha <ilan.hame@gmail.com>
2025-10-06 10:57:08 -0300

    testing right move logic fix

--- src/main/java/com/mauadev/code/Handler.java
+++ src/main/java/com/mauadev/code/Handler.java
@@ -174,20 +174,18 @@ public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIG
         boolean test = false;
 
         Coordinate cabeça = you.getHead();
-        // not colide with board and body
-        if(cabeça != null){
-            if(cabeça.getY()==board.getHeight()-1 || cabeça.getY()==you.getBody().get(1).getY()-1){
+        // not colide with board nor body
+        if(cabeça.getY()==board.getHeight()-1 || cabeça.getY()==you.getBody().get(1).getY()-1){
             cima = -1;
         }
         if(cabeça.getY()==0 || cabeça.getY()==you.getBody().get(1).getY()+1){
             baixo = -1;
         }
-        if(cabeça.getX()==board.getWidht()-1 || cabeça.getX()==you.getBody().get(1).getX()-1){
+        if(cabeça.getX()==board.getWidht() || cabeça.getX()==you.getBody().get(1).getX()-1){
             dir = -1;
         }
         if(cabeça.getX()==0 || cabeça.getX()==you.getBody().get(1).getX()+1){
             esq = -1;
-        }
         }
 
         //
